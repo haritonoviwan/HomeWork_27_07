65a4778270de2fbb433e4a1be88bcd6042af9203
Иван Харитонов <haritonoviwan@yandex.ru>
2023-07-28 00:38:53 +0400

    Сделала задание 23

---
+++ ex23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 23: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int counter = 1;
+int total = 1;
+int number = 0;
+try 
+{
+    Console.Write("Введите число: ");
+    number = int.Parse(Console.ReadLine() ?? "");
+}
+
+catch (Exception)
+{
+    Console.WriteLine("Ошибка ввода данных");
+    return;
+}
+
+Console.Write($"{number} -> ");
+
+while (counter <= number)
+{
+    total  = counter * counter * counter;
+    counter++;
+    Console.WriteLine(total);
+}
